3b61a3d7c342a471fc8f695cfa85482130189b71
programmermame <mameatpickout@gmail.com>
2024-05-29 18:45:45 +0300

    1

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,3 @@
-import getResponseFromAPI from './0-promise';
-function getResponseFromAPI() {
-    const response = getResponseFromAPI();
-    console.log(response instanceof Promise);
+export default function getResponseFromAPI() {
+    new Promise(() => { })
 }
