abf6a73abcff24d3a5d5cb1c2ed3459a32a685d3
programmermame <mameatpickout@gmail.com>
2024-06-06 06:08:45 +0300

    Typescript

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -8,14 +8,7 @@ export const weakMap = new WeakMap();
  */
 const MAX_ENDPOINT_CALLS = 5;
 
-/**
- * Tracks the number of calls made to an API's endpoint.
- * @param {{
- *   protocol: String,
- *   name: String,
- * }} endpoint - The endpoint to make a request to.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- */
+
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
