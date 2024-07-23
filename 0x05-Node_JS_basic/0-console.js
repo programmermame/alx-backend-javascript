e1e88f3a3891a6b296e8785094ae016960a2e83d
programmermame <mameatpickout@gmail.com>
2024-07-23 22:42:43 +0300

    NodeJS

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,4 +1,9 @@
-const displayMessage = (message) => {
-    console.log(message);
-}
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
 module.exports = displayMessage;
